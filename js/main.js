bcd9a183752fc35e7de748d00371c8bced3e06d7
Hackenstein <Hackenstein@users.noreply.github.com>
2019-03-16 10:50:43 -0500

    updated webook for today

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 
   function postToFeed(myParam) {
     $('#status').text(myParam);
-    $.post( "https://io.adafruit.com/api/v2/webhooks/feed/zdwpuA8HvJTMVo4nE6LVcfgi9hpf", { value: String(myParam)}, function( data, txtstatus ) {
+    $.post( "https://io.adafruit.com/api/v2/webhooks/feed/EDHnnAx3SJgiNUTCXr9FHVK42Dpn", { value: String(myParam)}, function( data, txtstatus ) {
       $('#statusResult').text("Data Loaded: " + data.completed_at );
     });
   };
