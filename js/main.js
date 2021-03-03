667c89da9eadc50d8701a5ff5f6bf993beb3cb35
Hackenstein <Hackenstein@users.noreply.github.com>
2021-03-02 20:28:25 -0600

    Update main.js
    updated hook

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 
   function postToFeed(myParam) {
     $('#status').text(myParam);
-    $.post( "https://io.adafruit.com/api/v2/webhooks/feed/DM2kkYvjfUdsKE2kwGxeKqA8Ybjk", { value: String(myParam)}, function( data, txtstatus ) {
+    $.post( "https://io.adafruit.com/api/v2/webhooks/feed/XTikQtwFvXvg2mgTtjMo8aBFhc7t", { value: String(myParam)}, function( data, txtstatus ) {
       $('#statusResult').text("Data Loaded: " + data.completed_at );
     });
   };
